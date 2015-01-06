da8aed014be67510febcfb222982012911dd0081
Thomas E. Bourgeat <thomas.bourgeat@ens.fr>
2015-01-06 14:49:07 +0100

    Work in progresss on environments

--- Project.v
+++ Project.v
@@ -8,9 +8,18 @@ Require Import Decidable.
 Local Open Scope nat_scope.
 
 Inductive typ := 
-  | var : nat -> typ
+  | vart : nat -> typ
   | arrow : typ -> typ -> typ
   | fall : nat -> typ -> typ.
+
+Fixpoint eq_typs (a:typ) (b:typ) : bool :=
+  match (a,b) with
+      | (vart i, vart j) => beq_nat i j
+      | (arrow a b,arrow c d) => andb (eq_typs a c) (eq_typs b d)
+      | (fall i t, fall j t') => eq_typs t t'
+(*We are not sure about the previous line*)
+      | (_,_) => false
+end.
 (*  Rank, secondePart*)
 
 (* Idea : de Bruinj indexes to carry bounded variables
@@ -20,84 +29,124 @@ a second nne and so on... *)
 
 
 (*The following function shift the Free variables by one*)
-Fixpoint shift (t:typ) (v:nat) : typ := 
+Fixpoint tshift (t:typ) (v:nat) : typ := 
    match t with
-       | var i   =>  var (if le_gt_dec v i then 1 + i else i  )
-       | arrow t1 t2  => arrow (shift t1 v) (shift t2 v) 
-       | fall rank sP  => fall rank (shift sP (v+1)) 
+       | vart i   =>  vart (if le_gt_dec v i then 1 + i else i  )
+       | arrow t1 t2  => arrow (tshift t1 v) (tshift t2 v) 
+       | fall rank sP  => fall rank (tshift sP (v+1)) 
    end.
 
 (*I have to write an example about that*)
 Fixpoint tsubst (t:typ) (v:nat) (newt :typ) := 
   match t with
-      | var l => (*several cases*)
+      | vart l => (*several cases*)
         if beq_nat v l then
           newt (*That's the variable to substitue*)
-        else var l 
+        else vart l 
           (*The following comments delimit a possible optimization : the compression
 of the namespace if we assume that's v is not in newt.*)
  
           (*if le_gt_dec l v (*That's a variable before the target variable*) 
              then
-               var l
+               vart l
              else (*That's a free variable, but we have a hole (we removed the variable) so we
                    need to garbage collect the name of this variable*)
-               var (l-1)
+               vart (l-1)
                *)
                
       | arrow t1 t2 => arrow (tsubst t1 v newt) (tsubst t2 v newt)
-      | fall rank sp => fall rank (tsubst sp (v+1) (shift newt 0)) 
+      | fall rank sp => fall rank (tsubst sp (v+1) (tshift newt 0)) 
                              
    end.
  
 
 Inductive term :=
-  | vart : nat -> term
+  | var : nat -> term
   | abst : typ -> term -> term
   | app : term -> term -> term
   | dept : nat -> term -> term
   | applt: term -> typ -> term.
 
+Fixpoint eq_terms (t:term) (t':term) :=
+  match (t,t') with
+    | (var a, var b) => beq_nat a b
+(*The following code is probably useless and may be not correct. We use a typeerasing semantic*)
+    | (abst tp trm, abst tp' trm') => eq_trms trm trm'
+                               
+    | (app trm1 trm2, app trm1' trm2')=> andb (eq_terms trm1 trm1') (eq_terms trm2 trm2')
+    | (dept _ trm, dept _ trm' ) => eq_terms trm trm'
+    | (applt trm tp, applt trm' tp') => eq_terms trm trm' 
 
-Fixpoint shiftTrmInTrm (t:term) (v:nat) : term :=
+Fixpoint shift (t:term) (v:nat) : term :=
    match t with
-       | vart i   =>  vart (if le_gt_dec v i then 1 + i else i  )
-       | abst tp trm  => abst tp (shiftTrmInTrm trm (v+1)) 
-       | app trm1 trm2  => app (shiftTrmInTrm trm1 v) (shiftTrmInTrm trm2 v)
-       | dept i trm => dept i (shiftTrmInTrm trm v) 
-       | applt trm tp =>  applt (shiftTrmInTrm trm v) tp
+       | var i   =>  var (if le_gt_dec v i then 1 + i else i  )
+       | abst tp trm  => abst tp (shift trm (v+1)) 
+       | app trm1 trm2  => app (shift trm1 v) (shift trm2 v)
+       | dept i trm => dept i (shift trm v) 
+       | applt trm tp =>  applt (shift trm v) tp
    end.
 
-Fixpoint shiftTpInTrm (t:term) (v:nat) : term :=
+Fixpoint shift_typ (t:term) (v:nat) : term :=
    match t with
-       | vart i   =>  vart (if le_gt_dec v i then 1 + i else i  )
-       | abst tp trm  => abst tp (shiftTpInTrm trm v) 
-       | app trm1 trm2  => app (shiftTpInTrm trm1 v) (shiftTpInTrm trm2 v)
-       | dept i trm => dept i (shiftTpInTrm trm (v+1)) 
-       | applt trm tp =>  applt (shiftTpInTrm trm v) tp
+       | var i   =>  var (if le_gt_dec v i then 1 + i else i  )
+       | abst tp trm  => abst tp (shift_typ trm v) 
+       | app trm1 trm2  => app (shift_typ trm1 v) (shift_typ trm2 v)
+       | dept i trm => dept i (shift_typ trm (v+1)) 
+       | applt trm tp =>  applt (shift_typ trm v) tp
    end.
 
-Fixpoint substTypInTerm (trm:term) (v:nat) (newt :typ) := 
+Fixpoint subst_typ (trm:term) (v:nat) (newt :typ) := 
   match trm with
-      | vart l => vart l 
-      | abst tp trm => abst (tsubst tp v newt) (substTypInTerm trm v newt)
-      | app trm1 trm2 => app (substTypInTerm trm1 v newt) (substTypInTerm trm2 v newt)
-      | dept i trm => dept i (substTypInTerm trm (v+1) (shift newt 0))
+      | var l => var l 
+      | abst tp trm => abst (tsubst tp v newt) (subst_typ trm v newt)
+      | app trm1 trm2 => app (subst_typ trm1 v newt) (subst_typ trm2 v newt)
+      | dept i trm => dept i (subst_typ trm (v+1) (tshift newt 0))
 (*We need to bound FTV correctly, so we shift each time we cross a forall*)
-      | applt trm tp => applt (substTypInTerm trm v newt) (tsubst tp v newt)
+      | applt trm tp => applt (subst_typ trm v newt) (tsubst tp v newt)
    end.
 
 
 
-Fixpoint substTermInTerm (trm:term) (v:nat) (newt : term) :=
+Fixpoint subst (trm:term) (v:nat) (newt : term) :=
   match trm with
-    | vart l => if beq_nat l v then newt
-                 else vart l
-    | abst tp trm => abst tp (substTermInTerm trm (v+1) (shiftTrmInTrm newt 0))
-    | app trm1 trm2 => app (substTermInTerm trm1 v newt) (substTermInTerm trm2 v newt)
-    | dept i trm => dept i (substTermInTerm trm v (shiftTpInTrm newt 0))
+    | var l => if beq_nat l v then newt
+                 else var l
+    | abst tp trm => abst tp (subst trm (v+1) (shift newt 0))
+    | app trm1 trm2 => app (subst trm1 v newt) (subst trm2 v newt)
+    | dept i trm => dept i (subst trm v (shift_typ newt 0))
     (* We need to shift FTV inside newt, to bound FTV correctly under new forallT *)
-    | applt trm tp => applt (substTermInTerm trm v newt) tp
+    | applt trm tp => applt (subst trm v newt) tp
   end.
+
+Require Import List.
+Import ListNotations.
+
+
+
+Inductive env_atom  :=
+| v : term -> typ -> env_atom
+|v_typ : typ -> nat -> env_atom.
+(*Note this definition could change*)
+
+(* Parameter environment : list env_atom. *)
+(*Question : how does it work?*)
+
+Fixpoint get_typ (t:term) (env: list env_atom) : option typ :=
+match env with
+    | nil => None
+    | cons (v trm tp) tl => if eq_trms trm t then
+                              Some tp
+                            else get_typ t tl
+    | cons (v_typ tp k) tl => get_typ t tl    
+end.
+
+Fixpoint get_kind (t:typ) (env: list env_atom) : option nat :=
+match env with
+    | nil => None
+    | cons (v trm tp) tl => get_typ t tl
+    | cons (v_typ tp k) tl => if eq_typs tp t then
+                                Some k
+                              else get_typ t tl    
+end.
 
 
