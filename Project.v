57586690298ec06271e3ddf7107e7ef1b574dd17
Thomas E. Bourgeat <thomas.bourgeat@ens.fr>
2015-01-24 11:06:44 +0100

    Cleaning

--- Project.v
+++ Project.v
@@ -143,12 +143,12 @@ Inductive env :=
  *)
 Fixpoint get_typ e (i:nat) :=
   match e with
-  | empty => None
-  | v_typ k tl => get_typ tl i
-  | v t tl => match i with
-              | 0 => Some t
-              | S x => get_typ tl x
-              end
+    | empty => None               
+    | v_typ k tl => get_typ tl i
+    | v t tl => match i with
+                  | 0 => Some t
+                  | S x => get_typ tl x
+                end
   end.
 
 (* The following function is used to get the kind of the type variable
@@ -156,21 +156,21 @@ Fixpoint get_typ e (i:nat) :=
  *)
 Fixpoint get_kind e (i:nat) :=
   match e with
-  | empty => None
-  | v_typ k tl => match i with
-                  | 0 => Some k
-                  | S x => get_kind tl x 
-                  end
+    | empty => None
+    | v_typ k tl => match i with
+                      | 0 => Some k
+                      | S x => get_kind tl x 
+                    end
 
-  | v t tl => get_kind tl i
+    | v t tl => get_kind tl i
   end.
-                  
+
 (*Comes from subject*)
 Fixpoint wf_typ (e : env) (T : typ) {struct T} : Prop :=
   match T with
-  | vart X      => get_kind e X <> None
-  | arrow T1 T2 => wf_typ e T1 /\ wf_typ e T2
-  | fall k T2   => wf_typ (v_typ k e) T2 
+    | vart X      => get_kind e X <> None
+    | arrow T1 T2 => wf_typ e T1 /\ wf_typ e T2
+    | fall k T2   => wf_typ (v_typ k e) T2 
   end.
 
 
@@ -178,134 +178,134 @@ Local Open Scope bool_scope.
 
 Fixpoint wf_typ_bool (e : env) (T : typ) {struct T} : bool :=
   match T with
-  | vart X      => match get_kind e X with | None => false | _ => true end
-  | arrow T1 T2 => wf_typ_bool e T1 && wf_typ_bool e T2
-  | fall k T2   => wf_typ_bool (v_typ k e) T2 
+    | vart X      => match get_kind e X with | None => false | _ => true end
+    | arrow T1 T2 => wf_typ_bool e T1 && wf_typ_bool e T2
+    | fall k T2   => wf_typ_bool (v_typ k e) T2 
   end
 .
 
 Theorem wf_typ_equiv : forall T e, wf_typ_bool e T = true <-> wf_typ e T.
 Proof.
-induction T; simpl; intros e.
-+ destruct (get_kind e n); intuition.
-  discriminate.
-+ rewrite andb_true_iff.
-  intuition; try (apply IHT1); try (apply IHT2); assumption.
-+ apply IHT; assumption.
+  induction T; simpl; intros e.
+  + destruct (get_kind e n); intuition.
+    discriminate.
+  + rewrite andb_true_iff.
+    intuition; try (apply IHT1); try (apply IHT2); assumption.
+  + apply IHT; assumption.
 Qed.
 
 Fixpoint wf_env (e : env) : Prop :=
   match e with
-  | empty     => True
-  | v T e     => wf_typ e T /\ wf_env e
-  | v_typ T e => wf_env e
+    | empty     => True
+    | v T e     => wf_typ e T /\ wf_env e
+    | v_typ T e => wf_env e
   end.
 
 Fixpoint wf_env_bool (e : env) : bool :=
   match e with
-  | empty     => true
-  | v T e     => andb (wf_typ_bool e T) (wf_env_bool e)
-  | v_typ T e => wf_env_bool e
+    | empty     => true
+    | v T e     => andb (wf_typ_bool e T) (wf_env_bool e)
+    | v_typ T e => wf_env_bool e
   end.
 
 Theorem wf_env_equiv : forall e, wf_env_bool e = true <-> wf_env e.
 Proof.
-induction e; simpl.
-+ intuition.
-+ assumption.
-+ rewrite andb_true_iff.
-  intuition; apply wf_typ_equiv; assumption.
+  induction e; simpl.
+  + intuition.
+  + assumption.
+  + rewrite andb_true_iff.
+    intuition; apply wf_typ_equiv; assumption.
 Qed.
 
 (* After Figure 5: Stratified System F Kinding Rules *)
 Inductive kinding (e : env) : typ -> nat -> Prop :=
 | kinded_var : forall X k p,
-                  get_kind e X = Some p ->
-                  p <= k ->
-                  wf_env e ->
-                  kinding e (vart X) k
+                 get_kind e X = Some p ->
+                 p <= k ->
+                 wf_env e ->
+                 kinding e (vart X) k
 | kinded_arrow : forall tp1 tp2 p q,
-                    kinding e tp1 p ->
-                    kinding e tp2 q ->
-                    kinding e (arrow tp1 tp2) (max p q)
+                   kinding e tp1 p ->
+                   kinding e tp2 q ->
+                   kinding e (arrow tp1 tp2) (max p q)
 | kinded_fall : forall k1 tp1 p,
-                   kinding (v_typ k1 e) tp1 p ->
-                   kinding e (fall k1 tp1) (1 + max p k1)
+                  kinding (v_typ k1 e) tp1 p ->
+                  kinding e (fall k1 tp1) (1 + max p k1)
 .
 
 (* This function computes the minimal kind for a type term *)
 Fixpoint kind (e : env) (tp : typ) : (option nat) :=
   match tp with
-  | vart X => if wf_env_bool e then get_kind e X else None
-  | Top.arrow tp1 tp2 => match (kind e tp1, kind e tp2 ) with
-                         | (Some p , Some q) => Some (max p q)
-                         | _ => None
-                         end
-  | fall k1 tp1 => match kind (v_typ k1 e) tp1  with
-                   | Some p => Some (1 + max p k1)
-                   | None => None
-                   end
+    | vart X => if wf_env_bool e then get_kind e X else None
+    | Top.arrow tp1 tp2 => match (kind e tp1, kind e tp2 ) with
+                             | (Some p , Some q) => Some (max p q)
+                             | _ => None
+                           end
+    | fall k1 tp1 => match kind (v_typ k1 e) tp1  with
+                       | Some p => Some (1 + max p k1)
+                       | None => None
+                     end
   end
 .
 
-Theorem soundness_of_kind : forall tp e k, kind e tp = Some k -> kinding e tp k.
+Theorem correctness_of_kind : forall tp e k, kind e tp = Some k -> kinding e tp k.
 Proof.
-induction tp; intros e k H; simpl in H.
-+ apply kinded_var with (p := k).
+  induction tp; intros e k H; simpl in H.
+  + apply kinded_var with (p := k).
   - destruct (wf_env_bool e); trivial.
     discriminate.
   - destruct (wf_env_bool e); trivial.
   - apply wf_env_equiv.
     destruct (wf_env_bool e); trivial.
     discriminate.
-+ specialize IHtp1 with (e := e).
-  specialize IHtp2 with (e := e).
-  destruct (kind e tp1) as [q1 | _]; try discriminate.
-  destruct (kind e tp2) as [q2 | _]; try discriminate.
-  inversion H as [h].
-  apply kinded_arrow.
+    + specialize IHtp1 with (e := e).
+      specialize IHtp2 with (e := e).
+      destruct (kind e tp1) as [q1 | _]; try discriminate.
+      destruct (kind e tp2) as [q2 | _]; try discriminate.
+      inversion H as [h].
+      apply kinded_arrow.
   - apply IHtp1; reflexivity.
   - apply IHtp2; reflexivity.
-+ specialize IHtp with (e := (v_typ n e)).
-  destruct (kind (v_typ n e)) as [p | _]; try discriminate.
-  inversion H as [h].
-  apply kinded_fall.
-  apply IHtp; reflexivity.
+    + specialize IHtp with (e := (v_typ n e)).
+      destruct (kind (v_typ n e)) as [p | _]; try discriminate.
+      inversion H as [h].
+      apply kinded_fall.
+      apply IHtp; reflexivity.
 Qed.
 
 Theorem completeness_of_kind :
   forall tp e k, (kinding e tp k) -> (exists p, p<=k /\ kind e tp = Some p).
 Proof.
-induction tp; intros e k kding; inversion kding.
-+ exists p.
-  intuition.
-  rewrite <- H.
-  simpl.
-  rewrite <- wf_env_equiv in H2.
-  rewrite H2.
-  rewrite H.
-  assumption.
-+ intuition.
-  simpl.
-  specialize IHtp1 with (e := e) (k := p).
-  specialize IHtp2 with (e := e) (k := q).
-  destruct (IHtp1 H1) as [p0 [h1 h2]].
-  destruct (IHtp2 H3) as [q0 [h3 h4]].
-  exists (max p0 q0).
-  split.
-  - apply Nat.max_le_compat; assumption.
-  - rewrite h2; rewrite h4; reflexivity.
-+ specialize IHtp with (e := (v_typ n e)) (k := p).
-  destruct (IHtp H2) as [p0 [h1 h2]].
-  exists (1 + max p0 n). 
-  split.
-  * simpl.
-    apply le_n_S.
-    apply Nat.max_le_compat_r.
-    assumption.
-  * simpl.
-    rewrite h2.
-    reflexivity.
+  induction tp; intros e k kding; inversion kding.
+  + exists p.
+           intuition.
+           rewrite <- H.
+           simpl.
+           rewrite <- wf_env_equiv in H2.
+           rewrite H2.
+           rewrite H.
+           assumption.
+           + intuition.
+             simpl.
+             specialize IHtp1 with (e := e) (k := p).
+             specialize IHtp2 with (e := e) (k := q).
+             destruct (IHtp1 H1) as [p0 [h1 h2]].
+             destruct (IHtp2 H3) as [q0 [h3 h4]].
+             exists (max p0 q0).
+             split.
+           - apply Nat.max_le_compat; assumption.
+           - rewrite h2; rewrite h4; reflexivity.
+             + specialize IHtp with (e := (v_typ n e)) (k := p).
+               destruct (IHtp H2) as [p0 [h1 h2]].
+               exists (1 + max p0 n). 
+               split.
+               * simpl.
+                 apply le_n_S.
+                 apply Nat.max_le_compat_r.
+                 assumption.
+               * simpl.
+                 rewrite h2.
+                 reflexivity.
 Qed. 
 
 (* After Figure 6: Stratified System F Type-Checking Rules *)
@@ -334,40 +334,40 @@ Inductive typing (e : env) : term -> typ -> Prop :=
    this is a strict inductive equality *)
 Fixpoint eq_typ t1 t2 : bool :=
   match (t1 , t2) with
-  | (vart x , vart y) => beq_nat x y
-  | (Top.arrow t11 t12 , Top.arrow t21 t22) => andb (eq_typ t11 t21) (eq_typ t12 t22)
-  | (fall k11 t12 , fall k22 t22) => andb (beq_nat k11 k22) (eq_typ t12 t22)
-  | _ => false
+    | (vart x , vart y) => beq_nat x y
+    | (Top.arrow t11 t12 , Top.arrow t21 t22) => andb (eq_typ t11 t21) (eq_typ t12 t22)
+    | (fall k11 t12 , fall k22 t22) => andb (beq_nat k11 k22) (eq_typ t12 t22)
+    | _ => false
   end
 .
 
 Theorem eq_typ_equiv: forall t1 t2, eq_typ t1 t2 = true <-> t1 = t2.
 Proof.
-induction t1; split; destruct t2; simpl; intro H; try discriminate.
-+ rewrite beq_nat_eq with (x := n) (y := n0); intuition.
-+ inversion H.
-  symmetry.
-  apply beq_nat_refl.
-+ apply andb_true_iff in H; destruct H as [H1 H2].
-  specialize IHt1_1 with (t2 := t2_1); destruct IHt1_1 as [iht1 _].
-  specialize IHt1_2 with (t2 := t2_2); destruct IHt1_2 as [iht2 _].
-  rewrite (iht1 H1).
-  rewrite (iht2 H2).
-  reflexivity.
-+ inversion H.
-  apply andb_true_iff; split.
+  induction t1; split; destruct t2; simpl; intro H; try discriminate.
+  + rewrite beq_nat_eq with (x := n) (y := n0); intuition.
+  + inversion H.
+    symmetry.
+    apply beq_nat_refl.
+  + apply andb_true_iff in H; destruct H as [H1 H2].
+    specialize IHt1_1 with (t2 := t2_1); destruct IHt1_1 as [iht1 _].
+    specialize IHt1_2 with (t2 := t2_2); destruct IHt1_2 as [iht2 _].
+    rewrite (iht1 H1).
+    rewrite (iht2 H2).
+    reflexivity.
+  + inversion H.
+    apply andb_true_iff; split.
   - specialize IHt1_1 with (t2 := t2_1); destruct IHt1_1 as [_ iht1].
     pattern t2_1 at 1; rewrite <- H1.
     intuition.
   - specialize IHt1_2 with (t2 := t2_2); destruct IHt1_2 as [_ iht2].
     pattern t2_2 at 1; rewrite <- H2.
     intuition.
-+ apply andb_true_iff in H; destruct H as [H1 H2].
-  specialize IHt1 with (t2 := t2); destruct IHt1 as [iht1 _].
-  rewrite (iht1 H2).
-  rewrite beq_nat_eq with (x := n) (y := n0); intuition.
-+ inversion H.
-  apply andb_true_iff; split.
+    + apply andb_true_iff in H; destruct H as [H1 H2].
+      specialize IHt1 with (t2 := t2); destruct IHt1 as [iht1 _].
+      rewrite (iht1 H2).
+      rewrite beq_nat_eq with (x := n) (y := n0); intuition.
+    + inversion H.
+      apply andb_true_iff; split.
   - symmetry; apply beq_nat_refl.
   - specialize IHt1 with (t2 := t2); destruct IHt1 as [_ iht1].
     rewrite H2 in iht1.
@@ -377,61 +377,61 @@ Qed.
 
 Fixpoint type (e : env) (trm : term) {struct trm} : option typ :=
   match trm with
-  | var x => if wf_env_bool e then get_typ e x else None
-  | abs tp1 trm1 => 
-    match kind e tp1 with
-    | Some a =>
-      match type (v tp1 e) trm1  with 
-      | None => None 
-      | Some tp2 => Some (Top.arrow tp1 tp2)
+    | var x => if wf_env_bool e then get_typ e x else None
+    | abs tp1 trm1 => 
+      match kind e tp1 with (*This match is not necessary but it is an historical artefact.*)
+        | Some a =>
+          match type (v tp1 e) trm1  with 
+            | None => None 
+            | Some tp2 => Some (Top.arrow tp1 tp2)
+          end
+        | None => None
       end
-    | None => None
-    end
-  | Top.app trm1 trm2 =>
-    match type e trm1  with
-    | Some (Top.arrow tp1 tp) =>
-      match type e trm2 with
-      | None => None
-      | Some tp_1 => if eq_typ tp1 tp_1 then Some tp else None
+    | Top.app trm1 trm2 =>
+      match type e trm1  with
+        | Some (Top.arrow tp1 tp) =>
+          match type e trm2 with
+            | None => None
+            | Some tp_1 => if eq_typ tp1 tp_1 then Some tp else None
+          end
+        | _ => None
       end
-    | _ => None
-    end
-  | dept k1 trm1 =>
-    match type (v_typ k1 e) trm1  with 
-    | None => None 
-    | Some tp1 => Some (fall k1 tp1) 
-    end
-  | applt trm1 tp2 =>
-    match type e trm1  with
-    | Some (fall k tp1) =>
-      match kind e tp2  with
-      | Some k1 => if beq_nat k1 k then Some (tsubst tp1 0 tp2) else None
-      | _ => None
-    end
-    | _ => None
-    end
+    | dept k1 trm1 =>
+      match type (v_typ k1 e) trm1  with 
+        | None => None 
+        | Some tp1 => Some (fall k1 tp1) 
+      end
+    | applt trm1 tp2 =>
+      match type e trm1  with
+        | Some (fall k tp1) =>
+          match kind e tp2  with
+            | Some k1 => if beq_nat k1 k then Some (tsubst tp1 0 tp2) else None
+            | _ => None
+          end
+        | _ => None
+      end
   end
 .
 
-Theorem soundness_of_type :
+Theorem correctness_of_type :
   forall trm tp e, type e trm = Some tp -> typing e trm tp.
 Proof.
-induction trm; intros tp e typ; simpl in typ.
-+ apply (typed_var). 
+  induction trm; intros tp e typ; simpl in typ.
+  + apply (typed_var). 
   - destruct (wf_env_bool e); trivial.
     discriminate.
   - apply wf_env_equiv.
     destruct (wf_env_bool e); trivial.
     discriminate.
-+ destruct (kind e t) eqn:eq1; try discriminate.
-  destruct (type (v t e) trm) eqn:eq; try discriminate.
-  inversion typ.
-  apply (typed_abs).
-  apply IHtrm; assumption.
-+ destruct (type e trm1) eqn:eq; try discriminate.
-  destruct (t) eqn:eq2; try discriminate.
-  destruct (type e trm2) eqn:eq1; try discriminate.
-  apply (typed_app e tp t0_1 trm1 trm2).
+    + destruct (kind e t) eqn:eq1; try discriminate.
+      destruct (type (v t e) trm) eqn:eq; try discriminate.
+      inversion typ.
+      apply (typed_abs).
+      apply IHtrm; assumption.
+    + destruct (type e trm1) eqn:eq; try discriminate.
+      destruct (t) eqn:eq2; try discriminate.
+      destruct (type e trm2) eqn:eq1; try discriminate.
+      apply (typed_app e tp t0_1 trm1 trm2).
   - apply (IHtrm1).
     rewrite eq.
     destruct (eq_typ t0_1 t0) eqn:eq3; try discriminate.
@@ -448,106 +448,26 @@ induction trm; intros tp e typ; simpl in typ.
       rewrite eq3; reflexivity.
     * apply eq_typ_equiv in eq3.
       rewrite eq3; reflexivity.
-+ destruct (type (v_typ n e) trm) eqn:eq; try discriminate.
-  destruct tp; try discriminate.
-  inversion typ.
-  rewrite <- H0.
-  apply (typed_dept e n trm tp).
-  apply IHtrm. 
-  rewrite <- H1.
-  assumption.
-+ destruct (type e trm) eqn:eq; try discriminate.
-  destruct t0; try discriminate.
-  destruct (kind e t) eqn:eq1; try discriminate.
-  destruct (beq_nat n0 n) eqn:eq2; try discriminate.
-  inversion typ.
-  apply (typed_applt e trm t0 t n).
+    + destruct (type (v_typ n e) trm) eqn:eq; try discriminate.
+      destruct tp; try discriminate.
+      inversion typ.
+      rewrite <- H0.
+      apply (typed_dept e n trm tp).
+      apply IHtrm. 
+      rewrite <- H1.
+      assumption.
+    + destruct (type e trm) eqn:eq; try discriminate.
+      destruct t0; try discriminate.
+      destruct (kind e t) eqn:eq1; try discriminate.
+      destruct (beq_nat n0 n) eqn:eq2; try discriminate.
+      inversion typ.
+      apply (typed_applt e trm t0 t n).
   - apply IHtrm.
     assumption.
-  - apply soundness_of_kind.
+  - apply correctness_of_kind.
     rewrite eq1.
     rewrite beq_nat_eq with (x := n0) (y := n); intuition.
 Qed.
-
-Inductive typ_le (e : env) : typ -> typ -> Prop :=
-| vart_le : forall X k,
-            get_kind e X = Some k ->
-            typ_le e (vart X) (vart X)
-| arrow_le : forall t11 t12 t21 t22,
-             typ_le e t11 t21 ->
-             typ_le e t12 t22 ->
-             typ_le e (arrow t11 t12) (arrow t21 t22)
-| fall_le : forall t1 t2 k1 k2,
-            typ_le (v_typ k1 e) t1 t2 ->
-            k1 <= k2 ->
-            typ_le e (fall k1 t1) (fall k2 t2)
-(* I´m not sure about the last case. *)
-.
-
-Lemma typ_le_refl : forall t1 e, wf_typ e t1 -> typ_le e t1 t1.
-induction t1; intros e lhs; simpl in lhs.
-+ destruct (get_kind e n) eqn:eq.
-  - apply vart_le with (k := n0); intuition.
-  - apply vart_le with (k := n); intuition.
-+ destruct lhs as [wf1 wf2].
-  apply arrow_le; intuition.
-+ specialize IHt1 with (e := v_typ n e).
-  apply fall_le; intuition.
-Qed.
-
-Lemma wf_env_all_typ :
-  forall e n tp, wf_env e -> get_typ e n = Some tp -> wf_typ e tp.
-Admitted.
-
-Lemma typing_wf_typ :
-  forall e trm tp, typing e trm tp -> wf_typ e tp.
-Admitted.
-
-Lemma typing_wf_env :
-  forall e trm tp, typing e trm tp -> wf_env e.
-Admitted.
-
-Lemma wf_env_typ :
-  forall e tp, wf_env (v tp e) -> wf_typ e tp.
-Proof.
-intros e tp H.
-simpl in H.
-intuition.
-Qed.
-
-Theorem completeness_of_type :
-  forall trm e tp1,
-  typing e trm tp1 ->
-  (exists tp2, typ_le e tp2 tp1 /\ type e trm = Some tp2).
-Proof.
-induction trm; intros e tp1 tping; inversion tping.
-+ exists tp1.
-  split.
-  - apply typ_le_refl.
-    apply wf_env_all_typ with (n := n); intuition.
-  - simpl.
-    rewrite <- wf_env_equiv in H1.
-    rewrite H1.
-    assumption.
-+ destruct (IHtrm (v t e) tp2 H2) as [tp [IH1 IH2]].
-  inversion tping.
-  exists (arrow t tp2).
-  split.
-  - apply arrow_le.
-    * apply typ_le_refl.
-      apply wf_env_typ.
-      apply typing_wf_env with (trm := trm) (tp := tp).
-      apply soundness_of_type; assumption.
-    * apply typ_le_refl.
-      assert (wf_typ e tp1).
-        apply typing_wf_typ with (trm := abs t trm); assumption.
-        rewrite <- H0 in H7; simpl in H7; intuition.
-  - 
-
-
-
-
-
 
 
 
